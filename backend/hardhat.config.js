320f2b745d2d466628ccbe86e87c1179480be804
nirban256 <nirban256@gmail.com>
2022-08-31 21:36:09 +0530

    adding modified Token, deployScript, hardhat.config files

--- backend/hardhat.config.js
+++ backend/hardhat.config.js
@@ -1,17 +1,28 @@
 require("@nomicfoundation/hardhat-toolbox");
+require('@nomiclabs/hardhat-etherscan');
 require('dotenv').config();
 
-const { API_URL, PRIVATE_KEY_1 } = process.env;
+const { API_URL, PRIVATE_KEY_1, API_KEY } = process.env;
 
 module.exports = {
   solidity: "0.8.9",
   networks: {
     hardhat: {},
-    testnet: {
+    bscTestnet: {
       url: API_URL,
       accounts: [PRIVATE_KEY_1]
     }
   },
+  paths: {
+    artifacts: "../frontend/src/artifacts",
+    cache: "../frontend/src/cache"
+  },
+  etherscan: {
+    apikey: {
+      bscTestnet: API_KEY
+    }
+  }
 };
 
-// Token contract address = 0x57219431E4272a080B60d6514571bEeCAC923337
+// Token contract address = 0x60f75Ef9cCEcE37907c2ea55f38AB109A9EcB18d
+// Staking contract address = 0xa8631959bfBCD9110e6B69fAcD0701c60b5D25A9
