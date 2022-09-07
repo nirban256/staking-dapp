61c372835363ec3e4d5d8661582414440152a449
nirban256 <nirban256@gmail.com>
2022-09-08 00:58:57 +0530

    added modified contract

--- backend/contracts/Staking.sol
+++ backend/contracts/Staking.sol
@@ -3,251 +3,209 @@ pragma solidity ^0.8.9;
 
 import "@openzeppelin/contracts/security/Pausable.sol";
 import "@openzeppelin/contracts/security/ReentrancyGuard.sol";
-import "@openzeppelin/contracts/utils/Context.sol";
 import "@openzeppelin/contracts/utils/math/SafeMath.sol";
 import "./Token.sol";
 
 contract Staking is Pausable, ReentrancyGuard {
-    Token ptkToken;
+    Token token;
 
     using SafeMath for uint256;
-    using SafeMath for uint128;
 
     address public admin;
+    uint256 private totalSupply;
 
-    uint256 public totalStakers;
-    uint256 public totalSupply;
-    address public rewardAddress;
-
-    struct StakeInfo {
-        uint256 startTS;
+    struct Stakers {
         uint256 amount;
+        uint256 startTime;
         uint256 claimed;
     }
 
-    struct User {
-        bool referred;
-        address referred_by;
-    }
+    mapping(address => Stakers) private stakerBalances;
 
-    struct Referal_levels {
-        uint256 level_1;
-        uint256 level_2;
-        uint256 level_3;
-        uint256 level_4;
-    }
-
-    mapping(address => Referal_levels) public refer_info;
-    mapping(address => User) public user_info;
+    address[] private stakersAddresses;
 
     event Staked(address indexed from, uint256 amount);
     event Claimed(address indexed from, uint256 amount);
 
-    mapping(address => StakeInfo) public stakeInfos;
-    mapping(address => bool) public addressStaked;
-
-    address[] public stakedAddresses;
-
-    constructor(
-        Token _tokenAddress,
-        address _rewardAddress,
-        address _admin
-    ) {
-        require(
-            address(_tokenAddress) != address(0),
-            "Token Address cannot be address 0"
-        );
-        ptkToken = _tokenAddress;
-        totalStakers = 0;
-        rewardAddress = _rewardAddress;
-        admin = _admin;
+    constructor(address _tokenContract) {
+        token = _tokenContract;
+        admin = msg.sender;
+        totalSupply = 0;
     }
 
     modifier onlyOwner() {
-        require(_msgSender() == admin, "You are not the admin");
+        require(msg.sender == admin, "You don't have the required permission");
         _;
     }
 
-    // function to transfer tokens into the contract
-    function transferToken(address _to, uint256 _amount) external onlyOwner {
-        require(ptkToken.transfer(_to, _amount), "Token transfer failed!");
+    function getTotalVolume() public view returns (uint256) {
+        return totalSupply;
     }
 
-    // function for getting referrals
-    function referee(address ref_add) public {
-        require(user_info[msg.sender].referred == false, " Already referred ");
-        require(ref_add != msg.sender, " You cannot refer yourself ");
-
-        user_info[msg.sender].referred_by = ref_add;
-        user_info[msg.sender].referred = true;
-
-        address level1 = user_info[msg.sender].referred_by;
-        address level2 = user_info[level1].referred_by;
-        address level3 = user_info[level2].referred_by;
-        address level4 = user_info[level3].referred_by;
-
-        if ((level1 != msg.sender) && (level1 != address(0))) {
-            refer_info[level1].level_1 += 1;
-        }
-        if ((level2 != msg.sender) && (level2 != address(0))) {
-            refer_info[level2].level_2 += 1;
-        }
-        if ((level3 != msg.sender) && (level3 != address(0))) {
-            refer_info[level3].level_3 += 1;
-        }
-        if ((level4 != msg.sender) && (level4 != address(0))) {
-            refer_info[level4].level_4 += 1;
-        }
+    function getStakersAddresses()
+        public
+        view
+        onlyOwner
+        returns (uint256[] memory)
+    {
+        return stakersAddresses;
     }
 
-    // function for calculating interests
-    function calculateInterest(address _account) internal returns (uint256) {
-        uint256 stakeAmount = stakeInfos[_msgSender()].amount;
-        uint256 totalTokens;
-        uint128 comissionAmount;
+    function getStakersBalances(address _account)
+        public
+        view
+        onlyOwner
+        returns (
+            uint256,
+            uint256,
+            uint256
+        )
+    {
+        return (
+            stakerBalances[_account].amount,
+            stakerBalances[_account].startTime,
+            stakerBalances[_account].claimed
+        );
+    }
 
-        // day 1
+    function stakeTokens(uint256 _amount) external {
+        require(_amount > 0, "Value must be greater than zero");
+        require(token.balanceOf(msg.sender) >= _amount, "Insufficient Balance");
+
+        token.transferFrom(msg.sender, address(this), _amount);
+
+        uint256 flag = 0;
+        for (uint256 i = 0; i < stakersAddresses.length; i++) {
+            if (stakersAddresses[i] == msg.sender) {
+                stakerBalances[i].amount += _amount;
+                flag++;
+                break;
+            }
+        } // Error: staking again without claiming will get to receive the caller of the contract more interest on the tokens inspite being staked later due to startTime property.
+
+        if (flag == 0) {
+            stakerBalances[msg.sender] = Stakers({
+                amount: _amount,
+                startTime: block.timestamp,
+                claimed: 0
+            });
+            stakersAddresses.push(msg.sender);
+        }
+
+        totalSupply += _amount;
+
+        emit Staked(msg.sender, _amount);
+    }
+
+    function calculateInterest(address _account, uint256 _amount)
+        internal
+        returns (uint256)
+    {
+        require(_account != address(0), "Owner cannot be the zero address");
+
+        uint256 totalTokens = 0;
+        uint256 comissionAmount = 0;
         if (
-            stakeInfos[_account].startTS + block.timestamp >=
-            stakeInfos[_account].startTS + 86400 &&
-            stakeInfos[_account].startTS + block.timestamp <
-            stakeInfos[_account].startTS + 172800
+            Stakers[_account].startTime + block.timestamp >=
+            Stakers[_account].startTime + 86400 &&
+            Stakers[_account].startTime + block.timestamp <
+            Stakers[_account].startTime + 172800
         ) {
-            totalTokens = ((stakeAmount * 2) / 100);
+            totalTokens = ((_amount * 2) / 100);
             comissionAmount = 100 * 10**9 wei;
         }
         // day 2
         else if (
-            stakeInfos[_account].startTS + block.timestamp >=
-            stakeInfos[_account].startTS + 172800 &&
-            stakeInfos[_account].startTS + block.timestamp <
-            stakeInfos[_account].startTS + 259200
+            Stakers[_account].startTime + block.timestamp >=
+            Stakers[_account].startTime + 172800 &&
+            Stakers[_account].startTime + block.timestamp <
+            Stakers[_account].startTime + 259200
         ) {
-            totalTokens = ((stakeAmount * 3) / 100);
+            totalTokens = ((_amount * 3) / 100);
             comissionAmount = 80 * 10**9 wei;
         }
         // day 3
         else if (
-            stakeInfos[_account].startTS + block.timestamp >=
-            stakeInfos[_account].startTS + 259200 &&
-            stakeInfos[_account].startTS + block.timestamp <
-            stakeInfos[_account].startTS + 345600
+            Stakers[_account].startTime + block.timestamp >=
+            Stakers[_account].startTime + 259200 &&
+            Stakers[_account].startTime + block.timestamp <
+            Stakers[_account].startTime + 345600
         ) {
-            totalTokens = ((stakeAmount * 4) / 100);
+            totalTokens = ((_amount * 4) / 100);
             comissionAmount = 70 * 10**9 wei;
         }
         // day 4
         else if (
-            stakeInfos[_account].startTS + block.timestamp >=
-            stakeInfos[_account].startTS + 345600 &&
-            stakeInfos[_account].startTS + block.timestamp <
-            stakeInfos[_account].startTS + 432000
+            Stakers[_account].startTime + block.timestamp >=
+            Stakers[_account].startTime + 345600 &&
+            Stakers[_account].startTime + block.timestamp <
+            Stakers[_account].startTime + 432000
         ) {
-            totalTokens = ((stakeAmount * 5) / 100);
+            totalTokens = ((_amount * 5) / 100);
             comissionAmount = 60 * 10**9 wei;
         }
         // day 5
         else if (
-            stakeInfos[_account].startTS + block.timestamp >=
-            stakeInfos[_account].startTS + 432000 &&
-            stakeInfos[_account].startTS + block.timestamp <
-            stakeInfos[_account].startTS + 518400
+            Stakers[_account].startTime + block.timestamp >=
+            Stakers[_account].startTime + 432000 &&
+            Stakers[_account].startTime + block.timestamp <
+            Stakers[_account].startTime + 518400
         ) {
-            totalTokens = ((stakeAmount * 6) / 100);
+            totalTokens = ((_amount * 6) / 100);
             comissionAmount = 50 * 10**9 wei;
         }
         // day 6
         else if (
-            stakeInfos[_account].startTS + block.timestamp >=
-            stakeInfos[_account].startTS + 518400 &&
-            stakeInfos[_account].startTS + block.timestamp <
-            stakeInfos[_account].startTS + 604800
+            Stakers[_account].startTime + block.timestamp >=
+            Stakers[_account].startTime + 518400 &&
+            Stakers[_account].startTime + block.timestamp <
+            Stakers[_account].startTime + 604800
         ) {
-            totalTokens = ((stakeAmount * 7) / 100);
+            totalTokens = ((_amount * 7) / 100);
             comissionAmount = 40 * 10**9 wei;
         }
         // day 7
         else if (
-            stakeInfos[_account].startTS + block.timestamp >=
-            stakeInfos[_account].startTS + 604800
+            Stakers[_account].startTime + block.timestamp >=
+            Stakers[_account].startTime + 604800
         ) {
-            totalTokens = ((stakeAmount * 8) / 100);
+            totalTokens = ((_amount * 8) / 100);
             comissionAmount = 30 * 10**9 wei;
         }
         // day 0
         else if (
-            stakeInfos[_account].startTS + block.timestamp <=
-            stakeInfos[_account].startTS + 86400
+            Stakers[_account].startTime + block.timestamp <=
+            Stakers[_account].startTime + 86400
         ) {
-            totalTokens = stakeAmount;
+            totalTokens = _amount;
             comissionAmount = 25 * 10**9 wei;
         }
 
-        totalSupply -= totalTokens;
         totalTokens -= comissionAmount;
 
         return totalTokens;
     }
 
-    // function to withdraw funds
-    function withdrawTokens() public payable {
+    function withdrawTokens(uint256 _amount) external whenNonReentrant {
         require(
-            addressStaked[_msgSender()] == true,
-            "You are not participated"
+            _amount <= stakerBalances[msg.sender].amount,
+            "You don't have sufficient funds"
         );
-        require(stakeInfos[_msgSender()].claimed == 0, "Already claimed");
 
-        uint256 amount = calculateInterest(_msgSender());
-        stakeInfos[_msgSender()].claimed = amount;
-        payable(_msgSender()).transfer(amount);
+        uint256 interestAmount = calculateInterest(msg.sender, _amount);
+        stakerBalances[msg.sender].claimed = _amount + interestAmount;
+        stakerBalances[msg.sender].amount -= _amount;
+        totalSupply -= _amount + interestAmount;
 
-        emit Claimed(_msgSender(), amount);
+        token.transferFrom(address(this), msg.sender, _amount + interestAmount);
 
-        addressStaked[_msgSender()] = false;
+        emit Claimed(address(this), _amount);
     }
 
-    // Function for staking tokens
-    function stakeToken(uint256 _stakeAmount) external payable whenNotPaused {
-        require(_stakeAmount > 0, "Stake amount should be correct");
-        require(
-            addressStaked[_msgSender()] == false,
-            "You already participated"
-        );
-        require(
-            ptkToken.balanceOf(_msgSender()) >= _stakeAmount,
-            "Insufficient Balance"
-        );
-
-        ptkToken.transferFrom(_msgSender(), address(this), _stakeAmount);
-        uint256 flag = 0;
-        for (uint256 i = 0; i < stakedAddresses.length; i++) {
-            if (stakedAddresses[i] == _msgSender()) {
-                flag++;
-            }
-        }
-        if (flag == 0) {
-            totalStakers++;
-            stakedAddresses.push(_msgSender());
-        }
-        ptkToken.transferFrom(msg.sender, address(this), _stakeAmount);
-        addressStaked[_msgSender()] = true;
-        totalSupply += _stakeAmount;
-
-        stakeInfos[_msgSender()] = StakeInfo({
-            startTS: block.timestamp,
-            amount: _stakeAmount,
-            claimed: 0
-        });
-
-        emit Staked(_msgSender(), _stakeAmount);
-    }
-
-    // function for pausing the contract in case of hacks
     function pause() external onlyOwner {
         _pause();
     }
 
-    // function to unpause the contract
     function unpause() external onlyOwner {
         _unpause();
     }
--- backend/contracts/Token.sol
+++ backend/contracts/Token.sol
@@ -1,13 +1,137 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.9;
 
-import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
+contract Token {
+    string public name;
+    string public symbol;
 
-contract Token is ERC20 {
-    uint8 constant _decimals = 18;
-    uint256 constant _totalSupply = 100 * (10**6) * 10**_decimals; // 100m tokens for distribution
+    uint8 private decimals;
+    uint256 private totalSupply;
+    address public owner;
 
-    constructor() ERC20("Potato Token", "PTK") {
-        _mint(msg.sender, _totalSupply);
+    mapping(address => uint256) private balances;
+    mapping(address => mapping(address => uint256)) private allowances;
+
+    event Transfer(address indexed from, address indexed to, uint256 amount);
+    event Approval(
+        address indexed _owner,
+        address indexed _spender,
+        uint256 amount
+    );
+
+    constructor(string memory _name, string memory _symbol) {
+        name = _name;
+        symbol = _symbol;
+        decimals = 18;
+        owner = msg.sender;
+
+        _mint(100 * (10**6) * 10**decimals); // 100m tokens for distribution
+    }
+
+    modifier onlyOwner() {
+        require(msg.sender == owner, "You are not the owner");
+        _;
+    }
+
+    function transferFrom(
+        address _from,
+        address _to,
+        uint256 _amount
+    ) external {
+        require(msg.sender != address(0), "transfer from the zero address");
+        require(_to != address(0), "transfer to the zero address");
+
+        uint256 fromBalance = balances[_from];
+        require(
+            fromBalance >= _amount,
+            "ERC20: transfer amount exceeds balance"
+        );
+        unchecked {
+            balances[_from] = fromBalance - _amount;
+            // Overflow not possible: the sum of all balances is capped by totalSupply, and the sum is preserved
+            // by decrementing then incrementing.
+            balances[_to] += _amount;
+        }
+
+        emit Transfer(_from, _to, _amount);
+    }
+
+    function approve(
+        address _owner,
+        address _spender,
+        uint256 _amount
+    ) public {
+        require(
+            msg.sender != address(0),
+            "ERC20: approve from the zero address"
+        );
+        require(_spender != address(0), "ERC20: approve to the zero address");
+
+        allowances[_owner][_spender] = _amount;
+        emit Approval(_owner, _spender, _amount);
+    }
+
+    function balanceOf(address _account) public view returns (uint256) {
+        return balances[_account];
+    }
+
+    function allowance(address _owner, address _spender)
+        public
+        view
+        returns (uint256)
+    {
+        return allowances[_owner][_spender];
+    }
+
+    function increaseAllowance(address _spender, uint256 addedValue)
+        public
+        virtual
+        returns (bool)
+    {
+        address _owner = msg.sender;
+        approve(_owner, _spender, allowance(_owner, _spender) + addedValue);
+        return true;
+    }
+
+    function decreaseAllowance(address spender, uint256 value)
+        public
+        virtual
+        returns (bool)
+    {
+        address _owner = msg.sender;
+        uint256 currentAllowance = allowance(_owner, spender);
+        require(currentAllowance >= value, "decreased allowance below zero");
+
+        unchecked {
+            approve(owner, spender, currentAllowance - value);
+        }
+
+        return true;
+    }
+
+    function _mint(uint256 _amount) public onlyOwner {
+        require(owner != address(0), "Owner cannot be the zero address");
+
+        totalSupply += _amount;
+        unchecked {
+            // Overflow not possible: balance + amount is at most totalSupply + amount, which is checked above.
+            balances[owner] += _amount;
+        }
+
+        emit Transfer(address(0), owner, _amount);
+    }
+
+    function _burn(address _account, uint256 _amount) external onlyOwner {
+        require(_account != address(0), "burn from the zero address");
+
+        uint256 accountBalance = balances[_account];
+        require(accountBalance >= _amount, "burn amount exceeds balance");
+        unchecked {
+            balances[_account] = accountBalance - _amount;
+            // Overflow not possible: amount <= accountBalance <= totalSupply.
+            totalSupply -= _amount;
+        }
+
+        emit Transfer(_account, address(0), _amount);
     }
 }
