6b34570c82493839e051d6dfc3f41efcd5fc77e1
nirban256 <nirban256@gmail.com>
2022-09-08 11:41:36 +0530

    test commit

--- backend/contracts/Staking.sol
+++ backend/contracts/Staking.sol
@@ -12,6 +12,7 @@ contract Staking is Pausable, ReentrancyGuard {
     using SafeMath for uint256;
 
     address public admin;
+    address private rewardAddress;
     uint256 private totalSupply;
 
     struct Stakers {
@@ -20,6 +21,22 @@ contract Staking is Pausable, ReentrancyGuard {
         uint256 claimed;
     }
 
+    struct User {
+        bool referred;
+        address referred_by;
+    }
+
+    struct Referal_levels {
+        uint256 level_1;
+        uint256 level_2;
+        uint256 level_3;
+        uint256 level_4;
+    }
+
+    mapping(address => Referal_levels) public refer_info;
+
+    // mappings for generating referral rewards
+    mapping(address => User) public user_info;
     mapping(address => Stakers) private stakerBalances;
 
     address[] private stakersAddresses;
@@ -27,9 +44,14 @@ contract Staking is Pausable, ReentrancyGuard {
     event Staked(address indexed from, uint256 amount);
     event Claimed(address indexed from, uint256 amount);
 
-    constructor(address _tokenContract) {
+    constructor(
+        address _tokenContract,
+        address _rewardAddress,
+        address _admin
+    ) {
         token = _tokenContract;
-        admin = msg.sender;
+        admin = _admin;
+        rewardAddress = _rewardAddress;
         totalSupply = 0;
     }
 
@@ -38,10 +60,12 @@ contract Staking is Pausable, ReentrancyGuard {
         _;
     }
 
+    // function to get the total number of tokens staked in the contract
     function getTotalVolume() public view returns (uint256) {
         return totalSupply;
     }
 
+    // function to get stakers address
     function getStakersAddresses()
         public
         view
@@ -51,6 +75,7 @@ contract Staking is Pausable, ReentrancyGuard {
         return stakersAddresses;
     }
 
+    // function to get information about the stakers
     function getStakersBalances(address _account)
         public
         view
@@ -68,6 +93,7 @@ contract Staking is Pausable, ReentrancyGuard {
         );
     }
 
+    // function to stake tokens
     function stakeTokens(uint256 _amount) external {
         require(_amount > 0, "Value must be greater than zero");
         require(token.balanceOf(msg.sender) >= _amount, "Insufficient Balance");
@@ -97,6 +123,7 @@ contract Staking is Pausable, ReentrancyGuard {
         emit Staked(msg.sender, _amount);
     }
 
+    // function to calculate interest of the tokens
     function calculateInterest(address _account, uint256 _amount)
         internal
         returns (uint256)
@@ -186,6 +213,7 @@ contract Staking is Pausable, ReentrancyGuard {
         return totalTokens;
     }
 
+    // function to withdraw tokens along with rewards
     function withdrawTokens(uint256 _amount) external whenNonReentrant {
         require(
             _amount <= stakerBalances[msg.sender].amount,
@@ -195,17 +223,46 @@ contract Staking is Pausable, ReentrancyGuard {
         uint256 interestAmount = calculateInterest(msg.sender, _amount);
         stakerBalances[msg.sender].claimed = _amount + interestAmount;
         stakerBalances[msg.sender].amount -= _amount;
-        totalSupply -= _amount + interestAmount;
+        totalSupply -= _amount;
 
         token.transferFrom(address(this), msg.sender, _amount + interestAmount);
 
         emit Claimed(address(this), _amount);
     }
 
+    // function to get referral rewards
+    function referee(address ref_add) public {
+        require(user_info[msg.sender].referred == false, " Already referred ");
+        require(ref_add != msg.sender, " You cannot refer yourself ");
+
+        user_info[msg.sender].referred_by = ref_add;
+        user_info[msg.sender].referred = true;
+
+        address level1 = user_info[msg.sender].referred_by;
+        address level2 = user_info[level1].referred_by;
+        address level3 = user_info[level2].referred_by;
+        address level4 = user_info[level3].referred_by;
+
+        if ((level1 != msg.sender) && (level1 != address(0))) {
+            refer_info[level1].level_1 += 1;
+        }
+        if ((level2 != msg.sender) && (level2 != address(0))) {
+            refer_info[level2].level_2 += 1;
+        }
+        if ((level3 != msg.sender) && (level3 != address(0))) {
+            refer_info[level3].level_3 += 1;
+        }
+        if ((level4 != msg.sender) && (level4 != address(0))) {
+            refer_info[level4].level_4 += 1;
+        }
+    }
+
+    // function to pause the contract in case of hacks
     function pause() external onlyOwner {
         _pause();
     }
 
+    // function to unpause the contract after any issue which occurs gets resolved
     function unpause() external onlyOwner {
         _unpause();
     }
